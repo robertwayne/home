2af14c59786803247bdd57c9cd7982bc22d45e27
Rob <13954303+robertwayne@users.noreply.github.com>
2020-07-13 19:42:45 -0400

    Greeting now changes based on my local time.
    Added Amazon to my key links.

---
+++ main.js
@@ -0,0 +1,13 @@
+document.getElementById('greeting').innerText = setGreeting()
+
+function setGreeting() {
+    let now =new Date().getHours()
+
+    if (now >= 4 && now <= 11) {
+        return 'morning'
+    } else if (now >= 12 && now <= 18) {
+        return 'afternoon'
+    } else {
+        return 'evening'
+    }
+}
