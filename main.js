092dffaa7741ef0b47f909f534b15377f32ebd96
Rob Wagner <rob@sombia.com>
2022-12-18 23:52:11 -0500

    Increase health check timer to 5 minutes

--- main.js
+++ main.js
@@ -1,41 +1,41 @@
-document.getElementById('greeting').innerText = setGreeting()
+document.getElementById("greeting").innerText = setGreeting()
 
 function setGreeting() {
     let now = new Date().getHours()
 
     if (now >= 4 && now <= 11) {
-        return 'morning'
+        return "morning"
     } else if (now >= 12 && now <= 18) {
-        return 'afternoon'
+        return "afternoon"
     } else {
-        return 'evening'
+        return "evening"
     }
 }
 
 function queryServiceHealth() {
-    const sites = ['spelldrop.gg', 'sombia.com']
+    const sites = ["spelldrop.gg", "sombia.com"]
 
     for (const site of sites) {
         const element = document.getElementById(site)
         fetch(`https://${site}/api/v1/health`)
             .then((response) => {
                 if (response.status === 200) {
-                    element.classList.remove('service-down')
-                    element.classList.add('service-up')
-                    element.innerHTML = 'up'
+                    element.classList.remove("service-down")
+                    element.classList.add("service-up")
+                    element.innerHTML = "up"
                 } else {
-                    element.classList.remove('service-up')
-                    element.classList.add('service-down')
-                    element.innerHTML = 'down'
+                    element.classList.remove("service-up")
+                    element.classList.add("service-down")
+                    element.innerHTML = "down"
                 }
             })
             .catch((_) => {
-                element.classList.remove('service-up')
-                element.classList.add('service-down')
-                element.innerHTML = 'down'
+                element.classList.remove("service-up")
+                element.classList.add("service-down")
+                element.innerHTML = "down"
             })
     }
 }
 
 queryServiceHealth()
-setInterval(queryServiceHealth, 30000)
+setInterval(queryServiceHealth, 300000)
